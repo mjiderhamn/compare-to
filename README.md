d4d69c5c2e87fd6d6a085e5844f3fd7df348f6f6
Mattias Jiderhamn <mattias.jiderhamn@expertsystems.se>
2013-10-10 20:38:26 +0200

    First draft

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
-compare
-=======
+# Better compareTo() syntax
 
-Better compareTo() syntax
+Improving readability for [`java.lang.Comparable.compareTo()`](http://docs.oracle.com/javase/7/docs/api/java/lang/Comparable.html#compareTo(T\))
+
+Example usage - expressive syntax 
+```java
+import static se.jiderhamn.CompareTo.is;
+
+...
+
+  if(is(1).equalTo(0)) {
+    ...
+  }
+
+  if(is(value1).lessThan(value2)) {
+    ...
+  }
+
+  if(is(a).lessThanOrEqualTo(b)) {
+    ...
+  }
+
+  if(is(date1).greaterThan(date2)) {
+    ...
+  }
+
+  if(is(a).greaterThanOrEqualTo(b)) {
+    ...
+  }
+```
+
+Example usage - short syntax 
+```java
+import static se.jiderhamn.CompareTo.is;
+
+...
+
+  if(is(1).eq(0)) {
+    ...
+  }
+
+  if(is(value1).lt(value2)) {
+    ...
+  }
+
+  if(is(a).le(b)) {
+    ...
+  }
+
+  if(is(date1).gt(date2)) {
+    ...
+  }
+
+  if(is(a).ge(b)) {
+    ...
+  }
+```
---
+++ src/test/java/se/jiderhamn/CompareToTest.java
@@ -0,0 +1,146 @@
+/*
+   Copyright 2012 Mattias Jiderhamn
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package se.jiderhamn;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static se.jiderhamn.CompareTo.is;
+
+/**
+ * Test cases for {@link se.jiderhamn.CompareTo}
+ * @author Mattias Jiderhamn
+ */
+public class CompareToTest {
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Equality
+
+  @Test
+  public void equalTo() {
+    // Integers
+    assertTrue(is(1).equalTo(1));
+    assertFalse(is(1).equalTo(0));
+    assertFalse(is(1).equalTo(2));
+    
+    // BigDecimal
+    assertTrue(is(new BigDecimal(1)).equalTo(new BigDecimal("1.0000000000")));
+  }
+  
+  @Test
+  public void eq() {
+    assertTrue(is(1).eq(1));
+    assertFalse(is(1).eq(0));
+    assertFalse(is(1).eq(2));
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Less
+  
+  @Test
+  public void lessThan() {
+    assertFalse(is(0).lessThan(-1));
+    assertFalse(is(0).lessThan(0));
+    assertTrue(is(0).lessThan(1));
+
+    assertFalse(is(BigDecimal.ZERO).lessThan(BigDecimal.ZERO.subtract(BigDecimal.ONE)));
+    assertFalse(is(BigDecimal.ZERO).lessThan(BigDecimal.ZERO));
+    assertTrue(is(BigDecimal.ZERO).lessThan(BigDecimal.ONE));
+  }
+
+  @Test
+  public void lessThanOrEqualTo() {
+    assertFalse(is(0).lessThanOrEqualTo(-1));
+    assertTrue(is(0).lessThanOrEqualTo(0));
+    assertTrue(is(0).lessThanOrEqualTo(1));
+
+    assertFalse(is(BigDecimal.ZERO).lessThanOrEqualTo(BigDecimal.ZERO.subtract(BigDecimal.ONE)));
+    assertTrue(is(BigDecimal.ZERO).lessThanOrEqualTo(BigDecimal.ZERO));
+    assertTrue(is(BigDecimal.ZERO).lessThanOrEqualTo(BigDecimal.ONE));
+  }
+
+  @Test
+  public void lt() {
+    assertFalse(is(0).lt(-1));
+    assertFalse(is(0).lt(0));
+    assertTrue(is(0).lt(1));
+
+    assertFalse(is(BigDecimal.ZERO).lt(BigDecimal.ZERO.subtract(BigDecimal.ONE)));
+    assertFalse(is(BigDecimal.ZERO).lt(BigDecimal.ZERO));
+    assertTrue(is(BigDecimal.ZERO).lt(BigDecimal.ONE));
+  }
+
+  @Test
+  public void le() {
+    assertFalse(is(0).le(-1));
+    assertTrue(is(0).le(0));
+    assertTrue(is(0).le(1));
+
+    assertFalse(is(BigDecimal.ZERO).le(BigDecimal.ZERO.subtract(BigDecimal.ONE)));
+    assertTrue(is(BigDecimal.ZERO).le(BigDecimal.ZERO));
+    assertTrue(is(BigDecimal.ZERO).le(BigDecimal.ONE));
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Greater
+  
+  @Test
+  public void greaterThan() {
+    assertTrue(is(0).greaterThan(-1));
+    assertFalse(is(0).greaterThan(0));
+    assertFalse(is(0).greaterThan(1));
+
+    assertTrue(is(BigDecimal.ZERO).greaterThan(BigDecimal.ZERO.subtract(BigDecimal.ONE)));
+    assertFalse(is(BigDecimal.ZERO).greaterThan(BigDecimal.ZERO));
+    assertFalse(is(BigDecimal.ZERO).greaterThan(BigDecimal.ONE));
+  }
+
+  @Test
+  public void greaterThanOrEqualTo() {
+    assertTrue(is(0).greaterThanOrEqualTo(-1));
+    assertTrue(is(0).greaterThanOrEqualTo(0));
+    assertFalse(is(0).greaterThanOrEqualTo(1));
+
+    assertTrue(is(BigDecimal.ZERO).greaterThanOrEqualTo(BigDecimal.ZERO.subtract(BigDecimal.ONE)));
+    assertTrue(is(BigDecimal.ZERO).greaterThanOrEqualTo(BigDecimal.ZERO));
+    assertFalse(is(BigDecimal.ZERO).greaterThanOrEqualTo(BigDecimal.ONE));
+  }
+
+  @Test
+  public void gt() {
+    assertTrue(is(0).gt(-1));
+    assertFalse(is(0).gt(0));
+    assertFalse(is(0).gt(1));
+
+    assertTrue(is(BigDecimal.ZERO).gt(BigDecimal.ZERO.subtract(BigDecimal.ONE)));
+    assertFalse(is(BigDecimal.ZERO).gt(BigDecimal.ZERO));
+    assertFalse(is(BigDecimal.ZERO).gt(BigDecimal.ONE));
+  }
+
+  @Test
+  public void ge() {
+    assertTrue(is(0).ge(-1));
+    assertTrue(is(0).ge(0));
+    assertFalse(is(0).ge(1));
+
+    assertTrue(is(BigDecimal.ZERO).ge(BigDecimal.ZERO.subtract(BigDecimal.ONE)));
+    assertTrue(is(BigDecimal.ZERO).ge(BigDecimal.ZERO));
+    assertFalse(is(BigDecimal.ZERO).ge(BigDecimal.ONE));
+  }
+}
